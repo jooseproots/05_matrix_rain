0c5359124ad3c2abdbf4e5022e5336a2f3eb1a2f
Joosep Roots <joosep.roots@gmail.com>
2024-03-19 15:12:35 +0100

    Updated variable names for readability

--- matrix_rain.py
+++ matrix_rain.py
@@ -8,33 +8,33 @@ import typer
 # Initializing cli
 app = typer.Typer()
 
-BLANK_CHAR = " "
-CLEAR_CHAR = "\x1b[H"
+blank_char = " "
+clear_char = "\x1b[H"
 
 # Drop state of each cell, specifies position in a "raindrop"
-STATE_NONE = 0
-STATE_FRONT = 1
-STATE_TAIL = 2
-STATE_MESSAGE = 3
+state_none = 0
+state_front = 1
+state_tail = 2
+state_message = 3
 
 # Drop lengths
-MIN_LEN = 5
-MAX_LEN = 12
+min_drop_length = 5
+max_drop_length = 12
 
 # Drop colours
-BODY_CLRS = [
+body_colors = [
     "\x1b[38;5;48m",
     "\x1b[38;5;41m",
     "\x1b[38;5;35m",
     "\x1b[38;5;238m",
 ]
-FRONT_CLR = "\x1b[38;5;231m"
-TOTAL_CLRS = len(BODY_CLRS)
+front_color = "\x1b[38;5;231m"
+total_colors = len(body_colors)
 
 class Matrix(list):
     def __init__(self, wait: int, glitch_freq: int, drop_freq: int, message_timer: int, messages: list[str]):
-        self.rows = 0
-        self.cols = 0
+        self.n_rows = 0
+        self.n_cols = 0
 
         self.wait = 0.06 / (wait / 100)
         self.glitch_freq = 0.01 / (glitch_freq / 100)
@@ -46,15 +46,15 @@ class Matrix(list):
         '''returns a string representation of the matrix, which is then printed to the terminal.'''
         text = ""
 
-        for (c, s, l) in sum(self[MAX_LEN:], []):
-            if s == STATE_NONE:
-                text += BLANK_CHAR
-            elif s == STATE_FRONT:
-                text += f"{FRONT_CLR}{c}"
-            elif s == STATE_TAIL:
-                text += f"{BODY_CLRS[l]}{c}"
+        for (char, state, length) in sum(self[max_drop_length:], []): # iterates over all cells
+            if state == state_none:
+                text += blank_char
+            elif state == state_front:
+                text += f"{front_color}{char}"
+            elif state == state_tail:
+                text += f"{body_colors[length]}{char}"
             else:
-                text += f"{BODY_CLRS[1]}{c}"
+                text += f"{body_colors[1]}{char}"
 
         return text
 
@@ -62,16 +62,17 @@ class Matrix(list):
         '''returns the size of the terminal prompt, which is used to adjust the matrix size if necessary.'''
         size = os.get_terminal_size()
 
-        return size.lines + MAX_LEN, size.columns
+        return size.lines + max_drop_length, size.columns
 
     @staticmethod
     def get_random_char():
-        return chr(random.choice([random.randint(32, 126), 9906, 985, 1993, 11439, 9880, 11801, 10047, 10048, 9753, 10086, 10087, 1126]))
+        special_characters = [9906, 985, 1993, 11439, 9880, 11801, 10047, 10048, 9753, 10086, 10087, 1126]
+        return chr(random.choice([random.randint(32, 126)] + special_characters))
 
     def update_cell(
         self,
-        r: int,
-        c: int,
+        i_row: int,
+        i_col: int,
         *,
         char: str = None,
         state: int = None,
@@ -79,128 +80,127 @@ class Matrix(list):
     ):
         '''updates the character, state, and length of a specific cell in the matrix.'''
         if char is not None:
-            self[r][c][0] = char
+            self[i_row][i_col][0] = char
 
         if state is not None:
-            self[r][c][1] = state
+            self[i_row][i_col][1] = state
 
         if length is not None:
-            self[r][c][2] = length
+            self[i_row][i_col][2] = length
 
     def fill(self):
         '''fills the matrix with random characters.'''
         self[:] = [
-            [[self.get_random_char(), STATE_NONE, 0] for _ in range(self.cols)]
-            for _ in range(self.rows)
+            [[self.get_random_char(), state_none, 0] for _ in range(self.n_cols)]
+            for _ in range(self.n_rows)
         ]
 
     def apply_glitch(self):
         '''applies random glitches to the matrix by changing the characters in random cells.'''
-        total = self.cols * self.rows * self.glitch_freq
+        total = self.n_cols * self.n_rows * self.glitch_freq
 
         for _ in range(int(total)):  # Chooses random column and row and updates character in that cell
-            c = random.randint(0, self.cols - 1)
-            r = random.randint(0, self.rows - 1)
+            i_col = random.randint(0, self.n_cols - 1)
+            i_row = random.randint(0, self.n_rows - 1)
 
-            self.update_cell(r, c, char=self.get_random_char())
+            self.update_cell(i_row, i_col, char=self.get_random_char())
 
     def message_glitch(self, seed: int):
         '''creates a vertical message in a random spot on the terminal by updating cells.
         Also returns the starting position (row and column) and the length of the message'''
         random.seed(seed)
         message = random.choice(self.messages)
-        c = random.randint(0, self.cols - 1)
-        r = random.randint(MAX_LEN, self.rows - len(message) - 1)
+        i_col = random.randint(0, self.n_cols - 1)
+        i_row = random.randint(max_drop_length, self.n_rows - len(message) - 1)
         for i in range(len(message)+1):
             if i == len(message):
-                self.update_cell(r+i, c, state=STATE_NONE)
+                self.update_cell(i_row+i, i_col, state=state_none) # makes last cell clear
             else:
-                self.update_cell(r+i, c, char=message[i], state=STATE_MESSAGE)
+                self.update_cell(i_row+i, i_col, char=message[i], state=state_message)
 
-        return r, c, len(message)        
+        return i_row, i_col, len(message)        
 
-    def delete_message(self, r, c, message_length):
+    def delete_message(self, i_row, i_col, message_length):
         '''deletes a message in the terminal by setting the specified matrix cells 
         to a blank state and giving them random characters'''
         for i in range(message_length + 1):
-            self.update_cell(r+i, c, char=self.get_random_char(), state=STATE_NONE, length=0)
+            self.update_cell(i_row+i, i_col, char=self.get_random_char(), state=state_none, length=0)
 
-    def drop_col(self, col: int):
+    def drop_col(self, i_col: int):
         '''drops a single column in the matrix by moving the characters down by one row, starting from the bottom-most row and working its way up.
         It also returns a boolean value indicating if the bottom-most cell in the given column was in the "front" state.'''
-        dropped = self[self.rows - 1][col] == STATE_FRONT # checking if the bottom-most cell in a given column has reached the "front" state and storing it as a boolean
+        dropped = self[self.n_rows - 1][i_col] == state_front # checking if the bottom-most cell in a given column has reached the "front" state and storing it as a boolean
 
-        for r in reversed(range(self.rows)): 
-            _, state, length = self[r][col]
+        for i_row in reversed(range(self.n_rows)): 
+            _, state, length = self[i_row][i_col]
 
-            if state == STATE_NONE:
+            if state == state_none:
                 continue
 
-            if r != self.rows - 1: 
-                self.update_cell(r + 1, col, state=state, length=length) 
+            if i_row != self.n_rows - 1: 
+                self.update_cell(i_row + 1, i_col, state=state, length=length) 
                 # move the cell down by one row by updating the cell in the row below it with the current character, state, and length.
 
-            self.update_cell(r, col, state=STATE_NONE, length=0) # Set the current cell to the "none" state and a length of 0.
+            self.update_cell(i_row, i_col, state=state_none, length=0) # Set the current cell to the "none" state and a length of 0.
 
         return dropped
 
-    def add_drop(self, row: int, col: int, length: int):
+    def add_drop(self, i_row: int, i_col: int, drop_length: int):
         '''adds new drops to the matrix, with a specified length and random starting position.'''
-        for i in reversed(range(length)):
-            r = row + (length - i)
+        for i in reversed(range(drop_length)):
+            r = i_row + (drop_length - i)
 
             if i == 0:
-                self.update_cell(r, col, state=STATE_FRONT, length=length) # specifies the front of the drop
+                self.update_cell(r, i_col, state=state_front, length=1) # specifies the front of the drop
             else:
-                l = math.ceil((TOTAL_CLRS - 1) * i / length)
-
-                self.update_cell(r, col, state=STATE_TAIL, length=l) # specifies the tail of the drop
+                l = math.ceil((total_colors - 1) * i / drop_length)
+                self.update_cell(r, i_col, state=state_tail, length=l) # specifies the tail of the drop
 
     def screen_check(self):
         '''checks the terminal size and adjusts the matrix size if necessary.'''
-        if (p := self.get_prompt_size()) != (self.rows, self.cols):
-            self.rows, self.cols = p
+        if (prompt_size := self.get_prompt_size()) != (self.n_rows, self.n_cols):
+            self.n_rows, self.n_cols = prompt_size
             self.fill()
 
     def update(self):
         '''updates the matrix by dropping columns and adding new drops.'''
-        dropped = sum(self.drop_col(c) for c in range(self.cols)) # drops all columns, sums number of drops that reached the end
+        dropped = sum(self.drop_col(i_col) for i_col in range(self.n_cols)) # drops all columns, sums number of drops that reached the end
 
-        total = self.cols * self.rows * self.drop_freq # total number of drops that should be in the matrix based on the drop_freq attribute
-        missing = math.ceil((total - dropped) / self.cols) # number of missing drops needed to reach the total number of drops.
+        total = self.n_cols * self.n_rows * self.drop_freq # total number of drops that should be in the matrix based on the drop_freq attribute
+        missing = math.ceil((total - dropped) / self.n_cols) # number of missing drops needed to reach the total number of drops.
 
         for _ in range(missing): # add number of missing drops to random columns
-            col = random.randint(0, self.cols - 1)
-            length = random.randint(MIN_LEN, MAX_LEN)
+            i_col = random.randint(0, self.n_cols - 1)
+            length = random.randint(min_drop_length, max_drop_length)
 
-            self.add_drop(0, col, length)
+            self.add_drop(0, i_col, length)
 
     def start(self):
         '''starts the animation loop, continuously updating and rendering the matrix.'''
-        iterations = 0
+        iteration = 0
         seed = None
 
         while True:
-            print(CLEAR_CHAR, end="")
+            print(clear_char, end="")
             print(self, end="", flush=True)
 
             self.screen_check()
             
             self.update()
 
-            if iterations == 0: 
+            if iteration == 0: 
                 seed = random.randint(0, 1000)  # Generates a new random seed
 
-            if iterations <= self.message_timer - 1: # Displays a message in the terminal for 'message_time' number of iterations
+            if iteration <= self.message_timer - 1: # Displays a message in the terminal for 'message_time' number of iterations
                 r, c, message_length = self.message_glitch(seed=seed) 
             else:
                 self.delete_message(r, c, message_length)
 
             self.apply_glitch()
 
+            iteration += 1
+            iteration %= self.message_timer + 1 # Resets iterations counter after specified number
             time.sleep(self.wait) # Time until new iteration
-            iterations += 1
-            iterations %= self.message_timer + 1 # Resets iterations counter after specified number
 
 
 @app.command() # makes start() a callable command for the Typer CLI application.
@@ -238,9 +238,10 @@ def start(
         if not 0 <= len(message) <= 40:
             raise typer.BadParameter("message too long to display")
 
-    matrix = Matrix(speed, glitches, frequency, message_timer, messages)
+    matrix = Matrix(speed, glitches, frequency, message_timer+20, messages)
     matrix.start()
 
 
 if __name__ == "__main__":
     app()
+# adjust terminal size to be large enough before running
